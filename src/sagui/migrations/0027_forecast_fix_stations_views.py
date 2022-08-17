bf299db5746620610ca0569e300a58111beac256
Jean Pommier <jean.pommier@pi-geosolutions.fr>
2022-08-17 17:03:55 +0200

    Remove duplicate definition

--- src/sagui/migrations/0027_forecast_fix_stations_views.py
+++ src/sagui/migrations/0027_forecast_fix_stations_views.py
@@ -12,46 +12,6 @@ class Migration(migrations.Migration):
     operations = [
         migrations.RunSQL(
             """
-
-DROP FUNCTION IF EXISTS guyane.func_stations_with_flow_previ() CASCADE;
-CREATE OR REPLACE FUNCTION guyane.func_stations_with_flow_previ()
-RETURNS TABLE(id bigint,
-			  name varchar(50), 
-			 river varchar(50),
-			 minibasin smallint,
-		     levels jsonb,
-			 geom geometry(Point,4326))
-AS $$
-DECLARE
-	dataset_tbl_name TEXT;
-	query1 TEXT;
-BEGIN
-	-- get the name of the dataset to use to match the thresholds. Can be default (assimilated) or defined in the saguiconfig table
-	SELECT 'guyane.hyfaa_data_' || COALESCE((SELECT use_dataset FROM guyane.sagui_saguiconfig LIMIT 1), 'assimilated') AS use_dataset
-	INTO dataset_tbl_name;
-	RAISE INFO 'dataset_tbl_name %', dataset_tbl_name;
-		
-	query1 := 'WITH stations AS (
-                SELECT s.id, s.name, s.river, s.minibasin_id AS minibasin_id, s.geom, d."date", 
-                    guyane.anomaly_to_alert_level(flow_anomaly) AS level
-                FROM guyane.hyfaa_stations s INNER JOIN %s d
-                    ON s.minibasin_id = d.cell_id
-                WHERE d."date" > (SELECT MAX("date") FROM %s) - ''10 days''::interval
-                ORDER BY s.name, d."date" DESC
-            )
-			SELECT id, name, river, minibasin_id, jsonb_agg(jsonb_build_object(''date'',"date",''level'',level)) AS levels, geom
-			FROM stations
-			GROUP BY id, name, river, minibasin_id, geom';
-	RETURN QUERY EXECUTE format(query1, dataset_tbl_name, dataset_tbl_name);
-END
-$$ LANGUAGE plpgsql;
-COMMENT ON FUNCTION guyane.func_stations_with_flow_previ() IS 
-'Append forecast levels on stations data';
-
-CREATE VIEW guyane.stations_with_flow_previ AS
-SELECT * FROM guyane.func_stations_with_flow_previ();               
-
-
 DROP FUNCTION IF EXISTS guyane.func_stations_with_flow_previ() CASCADE;
 CREATE OR REPLACE FUNCTION guyane.func_stations_with_flow_previ()
 RETURNS TABLE(id bigint,
