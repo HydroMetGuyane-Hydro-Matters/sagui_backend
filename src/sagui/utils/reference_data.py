a98a0088554573ac89fc1ad4d55491cd04b9dd64
Jean Pommier <jean.pommier@pi-geosolutions.fr>
2023-01-09 10:01:16 +0100

    Working flow for generating and importing reference data flow

--- src/sagui/utils/reference_data.py
+++ src/sagui/utils/reference_data.py
@@ -23,7 +23,7 @@ def setup_teardown_temp_tables(cursor: CursorWrapper):
     cursor.execute(
         '''
         DROP TABLE IF EXISTS stations_reference_flow_temp;
-        CREATE TEMPORARY TABLE stations_reference_flow_temp AS SELECT id, period_id, station_id, day_of_year, "value" FROM stations_reference_flow LIMIT 0;
+        CREATE TEMPORARY TABLE stations_reference_flow_temp AS SELECT id, period_id, station_id, day_of_year, flow FROM stations_reference_flow LIMIT 0;
         '''
     )
     try:
@@ -39,56 +39,56 @@ def setup_teardown_temp_tables(cursor: CursorWrapper):
 def import_csv(csv_file: Any):
     """
     Parse a CSV data file and try to import as much as possible into the DB (table stations_reference_flow)
-    Expected content for the csv file is
-        target_period;date;1;2;3;4;5;6;7;8;9;10;11;12;13
-        2010-2020;01/01/10;110;226;497;66;75;34;31;122;284;54;5;19;9
-        2010-2020;02/01/10;120;150;382;91;75;44;37;81;205;33;4;9;9
-        2010-2020;03/01/10;150;163;303;102;94;39;54;145;196;34;14;6;18
-        2010-2020;04/01/10;165;198;350;106;109;31;74;236;296;57;12;15;20
+    Expected content for the csv file is expected to be produced by the reference_data_from_MGB_run.py script
+    (key-value output) and comply to the following structure:
+        ref_period,mini,day_of_year,flow
+        1980-1990,3467,1,-59.0
+        1980-1990,3467,2,-61.3
+        1980-1990,3467,3,-61.5
+        1980-1990,3467,4,-60.9
+        1980-1990,3467,5,-59.5
         ...
-    target_period should already exist in the DB table stations_reference_flow_period
+    ref_period should already exist in the DB table stations_reference_flow_period
     :param csv_file: can be any input accepted by pandas read_csv function: file path, stream, ByteIO
     :return:
     """
     # Get list of available periods (declared in UI/DB)
     periods = models.StationsReferenceFlowPeriod.objects.all().values()
     period_names = [p['period'] for p in periods]
+
     # Get list of station ids (only allow to try to import those)
-    stations = models.Stations.objects.values('id', 'name').order_by('id')
-    stations_ids = [s['id'] for s in stations]
-    df = pd.read_csv(csv_file, sep=';', parse_dates=['date'],
-                     dayfirst=True, dtype={'target_period': "string", 'station_id': 'int16'})
+    stations = models.Stations.objects.values('id', 'name', 'minibasin').order_by('id')
+    df = pd.read_csv(csv_file, sep=',', dtype={
+        'ref_period': "string",
+        'mini': 'int16',
+        'day_of_year': 'int16',
+        'flow': 'float32',
+    })
 
     # filter out periods that are not valid (not declared)
-    df.query("target_period in " + str(period_names), inplace=True)
+    df.query("ref_period in " + str(period_names), inplace=True)
 
-    # insert a day of the year column
-    df['doy'] = df["date"].dt.dayofyear
-
-    # Group and take the mean
-    df_means_by_doy = df.groupby(['target_period', 'doy']).mean().round()
-
-    # Stack it into k,v form
-    df2 = df_means_by_doy.stack().reset_index()
-    df2.columns = ['target_period', 'day_of_year', 'station_id', 'value']
-    # convert value column to smallinteger type
-    df2['station_id'] = df2['station_id'].apply(np.int16)
-    df2['value'] = df2['value'].apply(np.int16)
-
-    # filter out stations that do not exist in the DB
-    df2.query("station_id in " + str(stations_ids), inplace=True)
+    # Round and convert flow values to int
+    df['flow'] = df['flow'].apply(np.int64)
 
     # Replace the target_periods column par a column with period_id referring to the periods table
     p = {i['period']: i['id'] for i in periods}
-    df2['period_id'] = df2['target_period'].map(p)
-    df2.drop('target_period', axis=1, inplace=True)
+    df['ref_period'] = df['ref_period'].map(p)
+
+    # filter out stations that do not exist in the DB
+    df.query("mini in " + str([s['minibasin'] for s in stations]), inplace=True)
+
+    # Replace the mini field by the station id
+    p = {i['minibasin']: i['id'] for i in stations}
+    df['mini'] = df['mini'].map(p)
+    df.rename({'ref_period': 'period_id', 'mini': 'station_id'}, axis=1, inplace=True)
 
     # Save to CSV file
-    # df2.to_csv('/tmp/df2.csv', index=False)
+    df.to_csv('/tmp/df2.csv', index=False)
 
     # save dataframe to an in memory buffer, cf https://naysan.ca/2020/05/09/pandas-to-postgresql-using-psycopg2-bulk-insert-performance-benchmark/
     buffer = StringIO()
-    df2.to_csv(buffer, columns=['period_id', 'station_id', 'day_of_year', 'value'], header=False, index=True)
+    df.to_csv(buffer, columns=['period_id', 'station_id', 'day_of_year', 'flow'], header=False, index=True)
     buffer.seek(0)
     # Write the data into the DB using copy from and an upsert query
     # Inspired from https://www.thebookofjoel.com/django-fast-bulk-upsert
@@ -97,10 +97,10 @@ def import_csv(csv_file: Any):
             cursor.copy_from(buffer, 'stations_reference_flow_temp', sep=",")
             cursor.execute(
                 '''
-                INSERT INTO stations_reference_flow (period_id, station_id, day_of_year, value)
-                SELECT t.period_id, t.station_id, t.day_of_year, t.value
+                INSERT INTO stations_reference_flow (period_id, station_id, day_of_year, flow)
+                SELECT t.period_id, t.station_id, t.day_of_year, t.flow
                 FROM stations_reference_flow_temp t
                 ON CONFLICT(period_id, station_id, day_of_year) DO UPDATE SET
-                    value = EXCLUDED.value
+                    flow = EXCLUDED.flow
                 '''
             )
---
+++ src/sagui/utils/reference_data_from_MGB_run.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import datetime
+import os
+
+'''
+Script de production des sé&ries de référence, à partir de la liste des valeurs quotidiennes, générées par un run MGB. 
+Aggrège les valeurs de débit par jour de l'année, pour chaque tranche de 10 ans
+Produit le résultat  sous deux formats : pivoté (une colonne par station) ou en mode key,value
+'''
+
+
+csv_file='/home/jean/PRO/projets_encours/2022-IRD-Hydromatters/sagui/data/stations/MGB-HYFAA_MSWEP_Guyane_1980-2020_Q_R.csv'
+conversion_factors='https://docs.google.com/spreadsheets/d/e/2PACX-1vSYUw5m-asL1Q_QHOvx6AO1NAzT-iuuSm8pxKjiAO4M2FvKIac97nT_yw9hkl0sNOYBgbo3WvW5Jvbn/pub?gid=0&single=true&output=csv'
+apply_conversion_factor = True
+
+parser = lambda date: datetime.datetime.strptime(date, '%Y-%m-%d')
+df = pd.read_csv(csv_file, sep=',', parse_dates=['date'], date_parser=parser, dtype={'Mini': 'int', 'Q':'float'})
+
+df.rename(columns={"Mini": "mini", "Q": "flow"}, inplace=True)
+
+# Filter out data that are too recent
+df = df[df['date'].dt.date < datetime.date(2020, 1, 1)]
+
+# Add the period column, computed from the dates
+def year_to_period(d):
+    import numpy
+    start_decade = int(numpy.floor(d/10))
+    end_decade = int(numpy.floor(d/10) + 1)
+    return '{}0-{}0'.format(start_decade, end_decade)
+year_to_period(1995)
+df['ref_period'] = df["date"].dt.year.apply(year_to_period)
+
+# Add the day-of-year column
+df['day_of_year'] = df["date"].dt.dayofyear
+
+# Calculate the mean value over the periods
+dff = df.groupby(['ref_period','mini', 'day_of_year']).mean()
+# dff['conv'] = dff['flow']
+
+if apply_conversion_factor:
+    # Apply conversion factors to match in-situ data
+    # We tap directly into the Google sheet, exposed as CSV
+    # Load the csv into a pandas dataframe
+    conv_fact = pd.read_csv(conversion_factors, sep=',', skip_blank_lines=True, dtype={'minibasin_mgb': 'str', 'conversion*****':'str'})
+    conv_fact = conv_fact[conv_fact['minibasin_mgb'] != 'Nan']
+    # Convert it into a dict, easier to use
+    conv_factors = dict()
+    conv_fact.reset_index()
+    for row in conv_fact.iterrows():
+        conv_factors[row[1]['minibasin_mgb']] = row[1]['conversion*****']
+
+    def apply_conv_factor(conv_factors: dict, minibasin_id: str, flow: float):
+        f = flow
+        val = eval(conv_factors[minibasin_id])
+        return val
+
+    # df['conv'] = apply_conv_factor(conv_factors, df['mini'], df['flow'])
+
+    for row in dff.iterrows():
+        mini = row[0][1]
+        # row[1]['conv'] = apply_conv_factor(conv_factors, str(mini), row[1]['flow'])
+        row[1]['flow'] = apply_conv_factor(conv_factors, str(mini), row[1]['flow'])
+
+df2 = dff.round(1)
+
+# Reshape the table
+df3 = df2.pivot_table(index=['ref_period','day_of_year'],columns='mini',values='flow')
+
+# Write back to CSV
+# Write key-value table
+out_file = os.path.splitext(csv_file)[0]+'_ref_data_kv.csv'
+df2.to_csv(out_file)
+print('key-value format written to {}'.format(out_file))
+# Write pivoted table
+out_file = os.path.splitext(csv_file)[0]+'_ref_data_pivot_table.csv'
+df3.to_csv(out_file)
+print('pivot-table format written to {}'.format(out_file))
