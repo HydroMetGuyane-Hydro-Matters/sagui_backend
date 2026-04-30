17efb17b56163e55665d8eec68c315b300998f12
Jean Pommier <jean.pommier@pi-geosolutions.fr>
2026-04-30 10:23:33 +0200

    fix: SQL query selects only recent data + implement upsert
    - There was an error in the dates manipulation in the SQL query, causing
    the whole set of data to be published everytime.
    - Publication has been updated with a less efficient but more polyvalent
    upsert. Code taken from hyaa_import.py.
    TODO: move the df_to_db publication code to a common util package, there
    is no need ot have it as a class function
    - implemented refresh_daysdelta like for hyaa_import: allows to force
      republish of recent files (day span given by this param). Allows to
    make sure we are replacing the data taken from Imerg analysis-early when
    better data is available.Kind of a hack. It would be better to keep
    track of the product_type, but would require deeper changes in the code.

--- src/sagui/management/commands/rainfall_import.py
+++ src/sagui/management/commands/rainfall_import.py
@@ -39,11 +39,14 @@ class Command(BaseCommand):
     # Class-wide variables, that will be set using env var or command options
     rootpath         = None
     db_connect_url   = None
+    db_schema        = None
+    db_table        = None
     force_update     = None
     only_last_n_days = None
     commit_page_size = None
     tablename        = 'sagui_rainfall'
     last_updated_without_errors = None
+    refresh_daysdelta: int = None
 
     def add_arguments(self, parser):
         parser.add_argument('-r', '--rootpath',
@@ -52,6 +55,12 @@ class Command(BaseCommand):
         parser.add_argument('-d', '--db_connect_url',
                             default=settings.SAGUI_SETTINGS.get('HYFAA_DATABASE_URI', None),
                             help='The connection URL for the DB. (Default: "postgresql://postgres:sagui@localhost:5432/sagui")')
+        parser.add_argument('-s', '--schema',
+                            default=settings.SAGUI_SETTINGS.get('HYFAA_DATABASE_SCHEMA', "guyane"),
+                            help='The database schema to target')
+        parser.add_argument('-t', '--table',
+                            default="sagui_rainfall",
+                            help='The target table')
         parser.add_argument('-f', '--force_update',
                             default=False,
                             action='store_true',
@@ -64,15 +73,22 @@ class Command(BaseCommand):
                             type=int,
                             default=settings.SAGUI_SETTINGS.get('HYFAA_IMPORT_COMMIT_PAGE_SIZE', 1),
                             help='Commit the data into the DB every n different dates (default 1). Should run faster if set to 10 or 50')
+        parser.add_argument('--refresh_daysdelta',
+                            type=int,
+                            default=3,
+                            help='To always update data from the recent days, set this to the number of days from now that you want to refresh. Defaults to 3) ')
 
     def handle(self, *args, **kwargs):
         tic = perf_counter()
 
         self.rootpath = kwargs['rootpath']
         self.db_connect_url = kwargs.get('db_connect_url')
+        self.db_schema = kwargs.get('schema')
+        self.db_table = kwargs.get('table')
         self.force_update = kwargs.get('force_update')
         self.only_last_n_days = kwargs.get('only_last_n_days')
         self.commit_page_size = kwargs.get('commit_page_size')
+        self.refresh_daysdelta = kwargs.get('refresh_daysdelta')
 
         self.stdout.write("Scanning folder {}".format(self.rootpath))
         new_files = self._get_files_list()
@@ -99,22 +115,33 @@ class Command(BaseCommand):
             concatenated_df = pd.concat([df, concatenated_df], ignore_index=True)
             if (counter % self.commit_page_size == 0) or (
                     f == new_files[-1]):  # last part means is last time element
-                try:
-                    # save dataframe to an in memory buffer, cf https://naysan.ca/2020/05/09/pandas-to-postgresql-using-psycopg2-bulk-insert-performance-benchmark/
-                    buffer = StringIO()
-                    concatenated_df.to_csv(buffer, header=False, index=False)
-                    buffer.seek(0)
+                # try:
+                #     # save dataframe to an in memory buffer, cf https://naysan.ca/2020/05/09/pandas-to-postgresql-using-psycopg2-bulk-insert-performance-benchmark/
+                #     buffer = StringIO()
+                #     concatenated_df.to_csv(buffer, header=False, index=False)
+                #     buffer.seek(0)
+                #
+                #     # Execute the query
+                #     with connection.cursor() as cursor:
+                #         cursor.copy_from(buffer, 'sagui_rainfall', sep=",", columns = ('cell_id','date', 'rain'))
+                #
+                # except (Exception, psycopg2.DatabaseError) as error:
+                #     print(error)
+                #     errors += 1
+                # finally:
+                #     # clear the list of records
+                #     concatenated_df = pd.DataFrame()
+                # dataframe to DB
+                e = self._publish_dataframe_to_db(concatenated_df)
+                if not e:
+                    self.stdout.write("Successfully published to DB")
+                else:
+                    self.stdout.write(self.style.ERROR(
+                        "Encountered a DB error when publishing data to DB"))
+                # count errors if there are
+                errors += e
 
-                    # Execute the query
-                    with connection.cursor() as cursor:
-                        cursor.copy_from(buffer, 'sagui_rainfall', sep=",", columns = ('cell_id','date', 'rain'))
-
-                except (Exception, psycopg2.DatabaseError) as error:
-                    print(error)
-                    errors += 1
-                finally:
-                    # clear the list of records
-                    concatenated_df = pd.DataFrame()
+                concatenated_df = pd.DataFrame()
 
             counter +=1
 
@@ -160,7 +187,7 @@ class Command(BaseCommand):
         # Execute a query over the sqlite DB
         conn = sqlite3.connect(self.rootpath)
         cur = conn.cursor()
-        cur.execute(f'''
+        q = f'''
             SELECT file_path, data_type, date_data, date_added_to_db, date_created, product_type, file_status, grid_status
             FROM (
               SELECT *,
@@ -173,11 +200,12 @@ class Command(BaseCommand):
                 ) AS rn
               FROM FILEINFO
               WHERE product_type IN ('analysis', 'analysis-early') AND
-                  DATE(date_data) > DATE('{last_updated_text}')
+                  DATE(date_data) > DATE('{last_updated_text}', '-{self.refresh_daysdelta} day')
             )
-            WHERE rn = 1 
+            WHERE rn = 1
             ORDER BY DATE(date_data);
-        ''')
+        '''
+        cur.execute(q)
         new_files=cur.fetchall()
         conn.close()
         filtered_new_files = [(f[0], f[2]) for f in new_files]
@@ -204,6 +232,42 @@ class Command(BaseCommand):
         }
         df = pd.DataFrame.from_dict(columns_dict)
         return df
+
+
+    def _publish_dataframe_to_db(self, df):
+        """
+        Publish the provided Pandas DataFrame into the DB.
+        Returns: - nb of errors if there were (0 if everything went well)
+        Params:
+          * df: pandas dataframe to publish
+          * tblname
+        """
+        try:
+            # Create a list of tuples from the dataframe values
+            tuples = [tuple(x) for x in df.to_numpy()]
+            # tuples = df.to_records(index=False).tolist() # seems faster but breaks the datetimes
+            # Comma-separated dataframe columns
+            cols = ','.join(list(df.columns))
+
+            # Run an upsert command (on conflict etc)
+            # Considers that the pkey is composed of the 2 first fields:
+            updatable_cols = list(df.columns)[2:]
+
+
+            # Write the update statement (internal part). EXCLUDED is a PG internal table contained rejected rows from the insert
+            # see https://www.postgresql.org/docs/10/sql-insert.html#SQL-ON-CONFLICT
+            externals = lambda n: "{n}=EXCLUDED.{n}".format(n=n)
+            update_stmt = ','.join(["%s" % (externals(name)) for name in updatable_cols])
+            query = "INSERT INTO {schema}.{table}({cols}) VALUES %s ON CONFLICT ON CONSTRAINT  rainfall_unique_cellid_day DO UPDATE SET {updt_stmt};".format(
+                schema=self.db_schema, table=self.db_table, cols=cols, updt_stmt=update_stmt)
+
+            # Execute the query
+            with connection.cursor() as cursor:
+                extras.execute_values(cursor, query, tuples)
+            return 0
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+            return 1
 
     @staticmethod
     def _datetime_from_filename(filename, regex):
